d3309328e98b69731c30f6375c51346ebd5306e5
ckstn0777 <ckstn0777@naver.com>
2022-09-17 01:07:57 +0900

    feat: backend setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Ah-Baegopa-Server
+## Ah-Baegopa-Server
+
+### 개요
+
 아 배고파 서버 with fastify
+
+### 개발 스택
+
+- node : 16.3.0
+- fastify: 4.6.0
+- typescript : 4.8.3
---
+++ index.js
@@ -0,0 +1,12 @@
+import fastify from 'fastify';
+const server = fastify();
+server.get('/ping', async (request, reply) => {
+    return 'pong';
+});
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
