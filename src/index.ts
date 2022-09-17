1dee0d9e97810d5f589c66483022c381308c1be7
ckstn0777 <ckstn0777@naver.com>
2022-09-17 14:19:07 +0900

    feat: add ts-node, nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 
-const server = fastify()
+const server = fastify({
+  logger: true,
+})
 
 server.get('/ping', async (request, reply) => {
-  return 'pong'
+  return 'pongdddss'
 })
 
 server.listen({ port: 8080 }, (err, address) => {
