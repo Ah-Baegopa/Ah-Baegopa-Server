66ac0188c17ecdf4d10d6d0ad8fe58917c316241
ckstn0777 <ckstn0777@naver.com>
2022-09-30 22:46:59 +0900

    feat: prisma 세팅 및 테스트 엔티티 생성, 사용

--- src/routes/api/auth/index.ts
+++ src/routes/api/auth/index.ts
@@ -2,11 +2,22 @@ import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import { FastifyPluginAsync } from 'fastify'
 import { loginSchema } from './schema.js'
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 const authRoute: FastifyPluginAsync = async (fastify) => {
   fastify
     .withTypeProvider<TypeBoxTypeProvider>()
     .post('/login', { schema: loginSchema }, async (request, reply) => {
       console.log(request.body.username, request.body.password)
+
+      const newUser = await prisma.user.create({
+        data: {
+          email: request.body.username,
+        },
+      })
+
+      console.log('newUser', newUser)
       return { token: 'world' }
     })
 }
